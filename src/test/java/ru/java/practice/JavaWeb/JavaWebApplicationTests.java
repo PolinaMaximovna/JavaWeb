5c21a6d179794051b6250e3cb0ecef2cf0c34312
polina <polina@example.ru>
2020-03-10 18:20:59 +0300

    Initial commit

---
+++ src/test/java/ru/java/practice/JavaWeb/JavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.practice.JavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
